1770a48eb5f26d619982b27eae6f0e79d19ce4ed
Alexander May <alexander.may@hjp-consulting.com>
2016-04-11 14:27:37 +0200

    Use icons to display status of GtServices

--- org.globaltester.service.ui/src/org/globaltester/service/ui/views/ServiceDashboardLabelProvider.java
+++ org.globaltester.service.ui/src/org/globaltester/service/ui/views/ServiceDashboardLabelProvider.java
@@ -1,11 +1,17 @@
 package org.globaltester.service.ui.views;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 import org.globaltester.service.GtService;
+import org.globaltester.service.ui.Activator;
+import org.osgi.framework.Bundle;
 
 /**
  * LabelProvider for the {@link ServiceDashboardView}
@@ -14,6 +20,45 @@ import org.globaltester.service.GtService;
  *
  */
 class ServiceDashboardLabelProvider extends LabelProvider implements ITableLabelProvider {
+
+	private static Image IMG_RUNNING = null;
+	private static Image IMG_STOPPED = null;
+	
+	private void initImages() {
+		Bundle bundle = Activator.getDefault().getBundle();
+		
+		if (IMG_RUNNING == null) {
+			Path path = new Path("icons/running.png");
+			URL url = FileLocator.find(bundle, path, null);
+
+			try {
+				IMG_RUNNING = new Image(null, url.openStream());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (IMG_STOPPED == null) {
+			Path path = new Path("icons/stopped.png");
+			URL url = FileLocator.find(bundle, path, null);
+
+			try {
+				IMG_STOPPED = new Image(null, url.openStream());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	public ServiceDashboardLabelProvider() {
+		initImages();
+	}
 
 	public String getColumnText(Object obj, int index) {
 		if (! (obj instanceof GtService)) return null;
@@ -34,14 +79,14 @@ class ServiceDashboardLabelProvider extends LabelProvider implements ITableLabel
 		GtService service = (GtService) obj;
 		
 		switch (index) {
-		case 0:
-			return getImage(service);
+		case 1:
+			if (service.isRunning()) {
+				return IMG_RUNNING;
+			} else {
+				return IMG_STOPPED;
+			}
 		default:
 			return null;
 		}
-	}
-
-	public Image getImage(Object obj) {
-		return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
 	}
 }
